7275a7ce25f61d9f8ca975b74151bbc9f591b1e7
li-cr <li-cr@a.com>
2025-04-08 22:59:16 +0800

    hashmaps, modules, string

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -22,6 +21,18 @@ use std::collections::HashMap;
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn add(has :&mut HashMap<String, Team>, team_name: String, goals_scored: u8, goals_conceded: u8) {
+    // Check if the team already exists in the HashMap
+    if let Some(team) = has.get_mut(&team_name) {
+        // If it exists, update the goals
+        team.goals_scored += goals_scored;
+        team.goals_conceded += goals_conceded;
+    } else {
+        // If it doesn't exist, create a new entry
+        has.insert(team_name.clone(), Team { goals_scored, goals_conceded });
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +50,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        add(&mut scores, team_1_name, team_1_score, team_2_score);
+        add(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
